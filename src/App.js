db69a951827e782499ac0b1ef683833d69510430
thyagomac <thyago.cormick@gmail.com>
2024-07-04 10:16:26 -0300

    jsut example on app.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [reverse, setReverse] = useState(false);
+  const logoClass = reverse ? 'reverse' : '';
+
+  const handleClick = () => {
+    setReverse((reverse) => !reverse);
+    //setReverse(!reverse);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
+        <img src={logo} className={`App-logo ${logoClass}`} alt="logo" />
+        <button onClick={handleClick}>Reverse</button>
       </header>
     </div>
   );
