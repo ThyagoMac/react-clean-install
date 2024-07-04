373273f2c766e380b044434d6390bfe92cde37ea
thyagomac <thyago.cormick@gmail.com>
2024-07-04 14:02:41 -0300

    hooks

--- src/App.js
+++ src/App.js
@@ -1,21 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
+const handleCounterTitleClick = () => {
+  console.log('handleCounterTitleClick');
+};
 
 function App() {
   const [reverse, setReverse] = useState(false);
+  const [counter, setCounter] = useState(0);
   const logoClass = reverse ? 'reverse' : '';
 
-  const handleClick = () => {
+  //componentDidUpdate
+  useEffect(() => {
+    handleConsoleLog('componentDidUpdate');
+  });
+  //componentDidMount
+  useEffect(() => {
+    const counterTitle = document.getElementById('counter-title');
+    counterTitle?.addEventListener('click', handleCounterTitleClick);
+    handleConsoleLog('componentDidMount');
+    //componentWillUmount
+    return () => {
+      handleConsoleLog('componentWillUmount');
+      counterTitle.removeEventListener('click', handleCounterTitleClick);
+    };
+  }, []);
+  //useEffect with dependences
+  useEffect(() => {
+    handleConsoleLog('useEffect with dependences counter: ', counter);
+  }, [counter]);
+
+  const handleConsoleLog = (str, val) => {
+    console.log('handleConsoleLog: ', str, val);
+  };
+
+  const handleReverse = () => {
     setReverse((reverse) => !reverse);
     //setReverse(!reverse);
+  };
+
+  const handleCounterIncrement = () => {
+    setCounter(counter + 1);
+  };
+  const handleCounterDecrement = () => {
+    setCounter(counter - 1);
   };
 
   return (
     <div className="App">
       <header className="App-header">
+        <div id="counter-title">Counter: {counter}</div>
         <img src={logo} className={`App-logo ${logoClass}`} alt="logo" />
-        <button onClick={handleClick}>Reverse</button>
+        <button onClick={handleReverse}>Reverse</button>
+        <br />
+        <button onClick={handleCounterIncrement}>Count +</button>
+        <button onClick={handleCounterDecrement}>Count -</button>
       </header>
     </div>
   );
